60b2fdd0c70227733a9f8e74cdd6ce5ec0e264e8
Garangbse <g.buke@alustudent.com>
2025-06-14 02:00:05 +0200

    baseline template but with contact_me resizing bug

--- homepage.js
+++ homepage.js
@@ -1,10 +1,18 @@
 const skills = document.getElementById("skills");
 const skills_content = document.getElementById("skills_content");
 const skills_content_button = document.getElementById("skills_content_button");
+const homepage = document.getElementById("homepage_id");
 
-skills.addEventListener("click",() => {skills_content.classList.add("show");
+// displays the skills box when pressed
+skills.addEventListener("click",() => {
+    skills_content.classList.add("show");
+    homepage_id.style.height = "1095px";
     console.log("skills has been pressed");
 });
-skills_content_button.addEventListener("click",() => {skills_content.classList.remove("show")
+
+// Closes the skills box when closed
+skills_content_button.addEventListener("click",() => {
+    skills_content.classList.remove("show")
+    homepage_id.style.height = "822px"
     console.log("body has been pressed")
 });
